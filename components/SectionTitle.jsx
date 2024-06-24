62b952398fec395c3b216891d46b534c69988a61
Thomas Price <thomasjprice2@gmail.com>
2024-06-24 11:32:33 +0100

    changed section title bg height

--- components/SectionTitle.jsx
+++ components/SectionTitle.jsx
@@ -2,7 +2,7 @@ const SectionTitle = ({ title, desc }) => {
   return (
     <div className="py-8 flex flex-col items-center">
       <h2 className="text-section font-semibold relative">{title}
-        <div className="w-[120%] h-10 absolute top-6 left-[50%] translate-x-[-50%] bg-primary bg-opacity-70 -z-10"></div>
+        <div className="w-[120%] h-8 absolute top-6 left-[50%] translate-x-[-50%] bg-primary bg-opacity-70 -z-10"></div>
       </h2>
 
       {desc && <p className="mt-6">{desc}</p>}
